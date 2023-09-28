e87d6e78c4dce4520b356148c0109cd7efcc33d9
Keith Valin <kvalin@redhat.com>
2023-09-28 14:48:26 -0400

    Add publisher

---
+++ include/topictalk/topic.hpp
@@ -0,0 +1,7 @@
+#ifndef TOPICTALK_TOPIC_HPP
+#define TOPICTALK_TOPIC_HPP
+
+#define TOPIC_NAME "topictalk_topic"
+#define MESSAGE_LENGTH 20
+
+#endif
--- src/topictalk_publisher.cpp
+++ src/topictalk_publisher.cpp
@@ -1,10 +1,49 @@
-#include <cstdio>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
-  printf("hello world topictalk package\n");
-  return 0;
+#include "topic.hpp"
+
+using namespace std::chrono_literals;
+
+#define MAX_ASCII 0x7f
+#define MIN_ASCII 0x20
+
+namespace TopicTalk {
+class Publisher : public rclcpp::Node {
+public:
+	Publisher() : Node("topictalk_publisher") {
+		this->_publisher = this->create_publisher<std_msgs::msg::String>(TOPIC_NAME, 10);
+
+		RCLCPP_DEBUG(this->get_logger(), "Opened %s topic", TOPIC_NAME);
+		this->_timer = this->create_wall_timer(500ms, std::bind(&Publisher::callback, this));
+		RCLCPP_DEBUG(this->get_logger(), "Created timer");
+	}
+private:
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher;
+	rclcpp::TimerBase::SharedPtr _timer;
+
+	void callback() {
+		auto message = std_msgs::msg::String();
+		std::string payload = "";
+		for(int i = 0; i < MESSAGE_LENGTH; ++i) {
+			payload.push_back((char) ((rand() % MAX_ASCII) + MIN_ASCII));
+		}
+		message.data = payload;
+		this->_publisher->publish(message);
+	}
+};
+
+}
+
+int main(int argc, char ** argv) {
+	rclcpp::init(argc, argv);
+	srand(time(NULL));
+	rclcpp::spin(std::make_shared<TopicTalk::Publisher>());
+	rclcpp::shutdown();
+	return 0;
 }

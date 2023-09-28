da2088585b41e81bdfecb605e82c7fd72819471f
Keith Valin <kvalin@redhat.com>
2023-09-28 16:26:30 -0400

    Update clock when message is last received

--- src/topictalk_listener.cpp
+++ src/topictalk_listener.cpp
@@ -18,13 +18,13 @@ public:
 		this->_sub = this->create_subscription<std_msgs::msg::Header>(TOPIC_NAME, 10, std::bind(&Subscriber::callback, this, _1));
 	}
     ~Subscriber() {
-        auto end = this->get_clock()->now();
-        RCLCPP_INFO(this->get_logger(), "Average: %f B/s", byte_counter/((end - this->_start_time).seconds()));
+        RCLCPP_INFO(this->get_logger(), "Average: %f B/s", byte_counter/((this->_last_msg - this->_start_time).seconds()));
     }
 private:
 	rclcpp::Subscription<std_msgs::msg::Header>::SharedPtr _sub;
     size_t byte_counter;
     rclcpp::Time _start_time;
+    rclcpp::Time _last_msg;
 
     void callback(const std_msgs::msg::Header &data) {
         size_t bytes_received = data.frame_id.length() + sizeof(data.stamp.sec)*2;
@@ -33,6 +33,7 @@ private:
         auto transmission_time = this->get_clock()->now() - recv_time;
         RCLCPP_INFO(this->get_logger(), "Received %ld bytes in %f seconds (%ld nanoseconds).  %f B/s", bytes_received, transmission_time.seconds(), transmission_time.nanoseconds(), bytes_received / transmission_time.seconds());
         this->byte_counter += bytes_received;
+        this->_last_msg = this->get_clock()->now();
     }
 };
 

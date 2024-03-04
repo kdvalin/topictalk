fa6f0f9e7a60f0fc453867fd15792214267164fe
Keith Valin <kvalin@redhat.com>
2024-03-04 16:06:28 -0500

    Added block size arg to the launchfile

--- launch.py
+++ launch.py
@@ -2,7 +2,7 @@ import os
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import TextSubstitution
+from launch.substitutions import TextSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
@@ -15,11 +15,14 @@ def generate_launch_description():
 	num_subscriptions = DeclareLaunchArgument(
 		"nsubs", default_value=TextSubstitution(text="1")
 	)
+	block_size = DeclareLaunchArgument(
+		"bs", default_value=TextSubstitution(text="4096")
+	)
 
 	return LaunchDescription([
 		num_topics,
 		num_publishers,
 		num_subscriptions,
-		Node(package="topictalk", executable="topictalk_publisher"),
+		Node(package="topictalk", executable="topictalk_publisher", parameters=[{"msg_length": LaunchConfiguration("bs")}]),
 		Node(package="topictalk", executable="topictalk_listener")
 	])

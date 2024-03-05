99a965fb9762fc02ab46dfbd7ef8e068998e92fe
Keith Valin <kvalin@redhat.com>
2024-03-05 09:30:21 -0500

    Expand out all launch arguments

--- launch.py
+++ launch.py
@@ -1,28 +1,42 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import TextSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 
+def declare_args():
+	return [
+		DeclareLaunchArgument(
+			"ntopics", default_value=TextSubstitution(text="1")
+		),
+		DeclareLaunchArgument(
+			"npubs", default_value=TextSubstitution(text="1")
+		),
+		DeclareLaunchArgument(
+			"nsubs", default_value=TextSubstitution(text="1")
+		),
+		DeclareLaunchArgument(
+			"nsubs", default_value=TextSubstitution(text="1")
+		),
+		DeclareLaunchArgument(
+			"bs", default_value=TextSubstitution(text="4096")
+		),
+	]
+
 def generate_launch_description():
-	num_topics = DeclareLaunchArgument(
-		"ntopics", default_value=TextSubstitution(text="1")
+	num_topics = LaunchConfiguration(
+		"ntopics", default=TextSubstitution(text="1")
 	)
-	num_publishers = DeclareLaunchArgument(
-		"npubs", default_value=TextSubstitution(text="1")
+	num_publishers = LaunchConfiguration(
+		"npubs", default=TextSubstitution(text="1")
 	)
-	num_subscriptions = DeclareLaunchArgument(
-		"nsubs", default_value=TextSubstitution(text="1")
+	num_subscriptions = LaunchConfiguration(
+		"nsubs", default=TextSubstitution(text="1")
 	)
-	block_size = DeclareLaunchArgument(
-		"bs", default_value=TextSubstitution(text="4096")
-	)
+	block_size = LaunchConfiguration("bs", default=TextSubstitution(text="4096"))
 
 	return LaunchDescription([
-		num_topics,
-		num_publishers,
-		num_subscriptions,
-		Node(package="topictalk", executable="topictalk_publisher", parameters=[{"msg_length": LaunchConfiguration("bs")}]),
+		Node(package="topictalk", executable="topictalk_publisher", parameters=[{"msg_length": block_size }]),
 		Node(package="topictalk", executable="topictalk_listener")
 	])
